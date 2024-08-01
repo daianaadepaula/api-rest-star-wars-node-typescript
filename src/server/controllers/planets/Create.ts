4d605863c1e8da95b809250c9252b5c0907fa23f
Daiana de Paula <daianaadepaula1@gmail.com>
2024-08-01 15:48:09 -0300

    feat: added yup validation to planets create controller

--- src/server/controllers/planets/Create.ts
+++ src/server/controllers/planets/Create.ts
@@ -1,11 +1,31 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
 
 interface IPlanet {
-	nome: string;
+	name: string;
 }
 
-export const create = (req: Request<{}, {}, IPlanet>, res: Response) => {
-	console.log(req.body.nome);
+const bodyValidation: yup.ObjectSchema<IPlanet> = yup.object().shape({
+	name: yup.string().required().min(3),
+});
+
+export const create = async (req: Request<{}, {}, IPlanet>, res: Response) => {
+	let validateData: IPlanet | undefined = undefined;
+
+	try {
+		validateData = await bodyValidation.validate(req.body);
+	} catch (error) {
+		const yupError = error as yup.ValidationError;
+
+		return res.json({
+			errors: {
+				default: yupError.message,
+			},
+		});
+	}
+
+	console.log(validateData);
 
 	return res.send('Create!');
 };
